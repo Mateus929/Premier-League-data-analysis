8892943b61a3c9c10b286ab76b8aeb62954b3844
BrilliantMate <zhorzholianimate@gmail.com>
2024-09-09 03:02:16 +0400

    Adding gradient_descent.py descent algorithm.

---
+++ models/gradient_descent.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+
+
+def cost_function(w, b, X_train, y_train):
+    assert (X_train.shape[0] == y_train.shape[0])
+    n = X_train.shape[0]
+    f_wb = 0
+    for i in range(n):
+        f_wb_i = np.dot(w, X_train[i]) + b - y_train[i]
+        f_wb_i = f_wb_i ** 2
+        f_wb += f_wb_i
+    f_wb /= 2 * n
+    return f_wb
+
+def gradient_function(w, b, X_train, y_train):
+    n = X_train.shape[0]
+    temp = np.dot(X_train, np.transpose(w))
+    f_vector = temp - y_train
+    dj_dw = np.dot(np.transpose(X_train), f_vector)
+    dj_dw /= n
+
+    dj_db = 0
+    for i in range(n):
+        dj_db_i = np.dot(w, X_train[i]) + b - y_train[i]
+        dj_db += dj_db_i
+    dj_db /= n
+
+    return dj_dw, dj_db
+
+def gradient_descent(X_train, y_train, learning_rate, max_iteration):
+    p = X_train.shape[1]
+    w, b = np.zeros(p), 0
+    previous_cost = float('inf')
+    for _ in range(max_iteration):
+        dw, db = gradient_function(w, b, X_train, y_train)
+        cur_cost = cost_function(w, b, X_train, y_train)
+        if np.all(dw == 0) and db == 0:
+            break
+        previous_cost = cur_cost
+        w -= learning_rate * dw
+        b -= learning_rate * db
+        if _ % 100 == 0:
+            print(f"Iteration {_}: w = {w}, b = {b}, cost = {cur_cost}")
+    return w, b
+
+
+# Load data
+X_train = pd.read_csv('../data/processed/gradient/X_train.csv').to_numpy()
+y_train = pd.read_csv('../data/processed/gradient/y_train.csv').values.flatten()
+
+w, b = gradient_descent(X_train, y_train, learning_rate=0.0007, max_iteration=10000)
+
+print("Weights:", w)
+print("Bias:", b)
+print("Cost function value:", cost_function(w, b, X_train, y_train))
---
+++ models/gradient_descent_process.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Load data
+df = pd.read_csv('../data/raw/matches.csv')
+
+# Load input data
+df['100_minus_poss'] = 100 - df['poss']
+X1 = df[['poss', 'gf']].copy()
+X1['goal'] = X1['gf']
+X2 = df[['100_minus_poss', 'ga']].copy()
+X2['poss'] = X2['100_minus_poss']
+X2['goal'] = X2['ga']
+X1.drop(columns='gf', inplace=True)
+X2.drop(columns='ga', inplace=True)
+X2.drop(columns='100_minus_poss', inplace=True)
+X = pd.concat([X1, X2], axis=0)
+
+# Load output data
+y1 = df[['xg']]
+y2 = df[['xga']].copy()
+y2['xg'] = y2['xga']
+y2.drop(columns='xga', inplace=True)
+y = pd.concat([y1, y2], axis=0)
+
+# split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+#save to raw files
+X_test.to_csv('../data/processed/gradient/X_test.csv', index=False)
+y_test.to_csv('../data/processed/gradient/y_test.csv', index=False)
+X_train.to_csv('../data/processed/gradient/X_train.csv', index=False)
+y_train.to_csv('../data/processed/gradient/y_train.csv', index=False)
+
+
+
